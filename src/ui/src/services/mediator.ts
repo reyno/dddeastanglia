55b969e0f4dee5a763fac05b348278cd648fe902
Russell Seamer <russell.seamer@reyno.co.uk>
2018-09-19 11:54:58 +0100

    save point

--- src/ui/src/services/mediator.ts
+++ src/ui/src/services/mediator.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from "aurelia-fetch-client";
+import { HttpClient, json } from "aurelia-fetch-client";
 import { autoinject } from "aurelia-framework";
+import environment from '../environment';
 
 @autoinject
 export class Mediator {
@@ -8,9 +9,27 @@ export class Mediator {
     private httpClient: HttpClient
     ) { }
 
-    send(name: string, request?: any): Promise<any> {
+    async send(name: string, request?: any): Promise<any> {
 
-
+      const response = await this.httpClient.fetch(`${environment.api}_mediator/${name}`, {
+        body: (!!request && json(request)) || undefined,
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        }
+      })
+  
+  
+      if (!response.ok) {
+        const error = await response.json();
+        if (!!error.validationErrors) {
+          return Promise.reject(new ValidationError(error));
+        } else {
+          return Promise.reject(new Error());
+        }
+      }
+  
+      return await response.json()
 
     }
 
