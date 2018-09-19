b79b710493c3cd4bca90fba240a2dda35e83f312
Russell Seamer <russell.seamer@reyno.co.uk>
2018-09-19 13:06:43 +0100

    save point

--- src/api/MediatR/PipelineBehavior.cs
+++ src/api/MediatR/PipelineBehavior.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Authorization;
+using System.Reflection;
+using DDDEastAnglia.Api.Exceptions;
 
 namespace DDDEastAnglia.Api.MediatR {
     public class PipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> {
+        private readonly HttpContext _httpContext;
         private readonly IServiceProvider _services;
 
         public PipelineBehavior(
+            IHttpContextAccessor httpContextAccessor,
             IServiceProvider services
             ) {
+            _httpContext = httpContextAccessor.HttpContext;
             _services = services;
         }
 
@@ -33,9 +42,30 @@ namespace DDDEastAnglia.Api.MediatR {
 
         }
 
-        private Task Authorise(TRequest request, CancellationToken cancellationToken) {
+        private async Task Authorise(TRequest request, CancellationToken cancellationToken) {
 
-            throw new NotImplementedException();
+            // Get required services
+            var schemeProvider = _httpContext.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
+            var policyEvaluator = _httpContext.RequestServices.GetRequiredService<IPolicyEvaluator>();
+            var policyProvider = _httpContext.RequestServices.GetRequiredService<IAuthorizationPolicyProvider>();
+
+
+            // get the default policy and use it to authenticate
+            var defaultPolicy = await policyProvider.GetDefaultPolicyAsync();
+            var authenticateResult = await policyEvaluator.AuthenticateAsync(defaultPolicy, _httpContext);
+
+
+            // get the authorize attributes from the request and combine
+            var requestPolicies = typeof(TRequest).GetCustomAttributes<AuthorizeAttribute>(inherit: true);
+            var authorizePolicy = await AuthorizationPolicy.CombineAsync(policyProvider, requestPolicies) ?? defaultPolicy;
+
+
+            // now do the authorization
+            var authorizeResult = await policyEvaluator.AuthorizeAsync(authorizePolicy, authenticateResult, _httpContext, resource: null);
+
+
+            // throw forbidden if not successful
+            if (!authorizeResult.Succeeded) throw new ForbiddenException();
 
         }
 
