6a041435952d949aaef1e39e3ca2f679259387c6
Russell Seamer <russell.seamer@reyno.co.uk>
2018-09-19 12:53:01 +0100

    save point

---
+++ src/api/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DDDEastAnglia.Api.Exceptions {
+    public class NotFoundException : Exception {
+        public NotFoundException() : base() { }
+        public NotFoundException(string message) : base(message) { }
+    }
+}
