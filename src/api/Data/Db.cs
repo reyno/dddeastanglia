f756f04316c8459e0b279d02ce25b252283d0cb8
Russell Seamer <russell.seamer@reyno.co.uk>
2018-09-20 09:50:19 +0100

    save point

--- src/api/Data/Db.cs
+++ src/api/Data/Db.cs
@@ -9,26 +9,6 @@ namespace DDDEastAnglia.Api.Data {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
-
-            modelBuilder
-                .Entity<Category>()
-                .HasData(
-                    new Category { Id = 1, Title = "Category 1" }
-                );
-
-            modelBuilder
-                .Entity<Product>()
-                .HasData(
-                    new { Id = 1, Title = "Product 1", CategoryId = 1 },
-                    new { Id = 2, Title = "Product 2", CategoryId = 1 },
-                    new { Id = 3, Title = "Product 3", CategoryId = 1 }
-                );
-
-
-            base.OnModelCreating(modelBuilder);
-                
-        }
-
+        
     }
 }
