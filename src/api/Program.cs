f86d41496b1639074d690a89dac4eb69cd418765
Russell Seamer <russell.seamer@reyno.co.uk>
2018-09-20 06:12:41 +0100

    save point

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DDDEastAnglia.Api.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
-namespace DDDEastAnglia.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace DDDEastAnglia.Api {
+    public class Program {
+        public static async Task Main(string[] args) {
+
+            var webHost = CreateWebHostBuilder(args).Build();
+
+            // make sure we have one category for the demo
+            using(var scope = webHost.Services.CreateScope()) {
+                var db = scope.ServiceProvider.GetService<Db>();
+                if(!await db.Categories.AnyAsync(x => x.Title == "Category 1")) {
+                    db.Categories.Add(new Data.Entities.Category {
+                        Title = "Category 1"
+                    });
+                    await db.SaveChangesAsync();
+                }
+
+            }
+
+            await webHost.RunAsync();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
