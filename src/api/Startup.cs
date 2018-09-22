a6de309877fd0aa9faa1929da5e77db43d9fe0cf
Russell Seamer <russell.seamer@reyno.co.uk>
2018-09-22 09:11:54 +0100

    save point

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -40,10 +40,9 @@ namespace DDDEastAnglia.Api {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            } else {
-                app.UseHsts();
             }
 
             app.UseCors(config => config
@@ -53,8 +52,8 @@ namespace DDDEastAnglia.Api {
                 .AllowCredentials()
             );
 
-            app.UseHttpsRedirection();
             app.UseMvc();
+
         }
     }
 }
